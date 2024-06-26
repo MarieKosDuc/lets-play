81d17e091e55177efb89683f45cc0df0864a888e
Marie Koscianski-Ducharlet <marie.koscianski@gmail.com>
2024-06-26 19:04:11 +0200

    Feat/Footer pages, CGU acceptance in signup

--- letsplay-backend/src/main/java/com/mariekd/letsplay/authentication/repositories/UserRepository.java
+++ letsplay-backend/src/main/java/com/mariekd/letsplay/authentication/repositories/UserRepository.java
@@ -2,7 +2,11 @@ package com.mariekd.letsplay.authentication.repositories;
 
 import com.mariekd.letsplay.authentication.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -16,4 +20,9 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByName (String name);
 
     User findByName(String name);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM app_user WHERE user_id = :id", nativeQuery = true)
+    void deleteById(@Param("id") UUID id);
 }
